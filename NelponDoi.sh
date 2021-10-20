f60d2d587b60a937f98836c34de99ed1568fe9b0
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-10-20 19:05:18 +0700

    Create NelponDoi.sh

---
+++ NelponDoi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+figlet Spam | lolcat
+figlet Nelpon | lolcat
+echo "==========================================="
+echo "Author : Mr.T1T4N"
+echo "Team : Indonesia Cyber | Linux Cyber"
+echo "Youtube : T1T4N TZY"
+echo "==========================================="
+echo
+echo "==========================================="
+echo "1). Spam Sekarang"
+echo "99). Keluar"
+echo "==========================================="
+read -p "PilihLah Sesuka Kalian :" who
+
+if [ $who = 1 ]
+then
+read -p "Spam Sekarang :" nomor
+link=https://id.jagreward.com/member/verify-mobile/$n>
+curl -S $link
+fi
